f4d064389fadefd2b1a760c9d0ac63c0ae0a08aa
VictorNoe <20213tn059@utez.edu.mx>
2024-03-18 17:39:31 -0600

    Fix render image and validate pdf

--- src/utils/pdf/pdf.js
+++ src/utils/pdf/pdf.js
@@ -1,12 +1,19 @@
 import {useAuth} from "../../context/AuthContext";
 
-async function pdfHtml1 (dataProyect, datas, total){
+const pdfHtml = (dataProyect, datas, total) => {
 
     const { dataUser } = useAuth();
     const currentDate = new Date();
     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
     const formattedDate = currentDate.toLocaleDateString('es-ES', options);
 
+    const formatCurrency = (amount) => {
+        const parts = amount.toFixed(2).toString().split('.');
+        const integerPart = parts[0];
+        const decimalPart = parts[1];
+        const formattedIntegerPart = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+        return '$' + formattedIntegerPart + '.' + decimalPart;
+    }
     return `
     <!DOCTYPE html>
     <html lang="es">
@@ -32,7 +39,7 @@ async function pdfHtml1 (dataProyect, datas, total){
             }
 
             .header {
-                text-align: center;
+                text-align: justify;
                 margin-bottom: 20px;
             }
 
@@ -95,8 +102,7 @@ async function pdfHtml1 (dataProyect, datas, total){
             }
 
             .total {
-                margin-top: 20px;
-                float: right;
+                text-align: end;
                 font-size: 24px;
                 font-weight: bold;
                 color: #094b4d;
@@ -139,16 +145,16 @@ async function pdfHtml1 (dataProyect, datas, total){
             <tbody>
                 ${datas.map(dat => `
                     <tr>
-                        <td>${dat.Concepto}</td>
+                        <td style="text-align: start">${dat.Concepto}</td>
                         <td>${dat.Unidad}</td>
-                        <td>${dat.Cantidad}</td>
-                        <td>$${dat.PU}</td>
-                        <td>$${dat.Importe}</td>
+                        <td>${parseFloat(dat.Cantidad).toFixed(2)}</td>
+                        <td>${formatCurrency(parseFloat(dat.PU))}</td>
+                        <td>${formatCurrency(parseFloat(dat.Importe))}</td>
                     </tr>`).join('')}
             </tbody>
         </table>
         <div class="total">
-            <p><strong>Total:</strong> $${parseFloat(total).toFixed(2)}</p>
+            <p><strong>Total:</strong> ${formatCurrency(parseFloat(total))}</p>
         </div>
     </div>
 
@@ -158,5 +164,5 @@ async function pdfHtml1 (dataProyect, datas, total){
 }
 
 module.exports = {
-    pdfHtml1
+    pdfHtml
 };
